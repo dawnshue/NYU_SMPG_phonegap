db14ddc81d689abb1e1483e6f02519c5031e5e08
Vangie Shue <vangieshue@gmail.com>
2014-12-19 14:15:14 -0500

    Update index.js

--- www/index.js
+++ www/index.js
@@ -80,14 +80,14 @@
     }
     // Android and Amazon Fire OS
     function onNotification(e) {
-      // alert('RECEIVED:' + JSON.stringify(e));
+      alert('RECEIVED:' + JSON.stringify(e));
       switch( e.event )
       {
         case 'registered':
           if ( e.regid.length > 0 )
           {
             // Your GCM push server needs to know the regID before it can push to this device
-            // alert('REGID:' + e.regid);
+            alert('REGID:' + e.regid);
             window.regid = e.regid;
             sendNotification({regid: e.regid}, "");
             //save registration id
